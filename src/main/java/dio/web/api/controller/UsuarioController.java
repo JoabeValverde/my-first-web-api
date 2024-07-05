f19356c275e0648d51c0fba636ed81799acb7fe9
JoabeValverde <joabe23@hotmail.com>
2024-07-05 12:16:47 -0300

    UPDATE: Add os metodos deleteUser e postUser em Usuario Controler e testando as anotacoes @DeleteMapping e @PostMapping usando a API Client Postman

--- src/main/java/dio/web/api/controller/UsuarioController.java
+++ src/main/java/dio/web/api/controller/UsuarioController.java
@@ -3,18 +3,30 @@ package dio.web.api.controller;
 import dio.web.api.model.Usuario;
 import dio.web.api.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users") //  quando temos a necessidade de centralizar um prefixo
 public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
-    @GetMapping("/users")
+    @GetMapping()
     public List<Usuario>getUsers(){
-        return repository.fingAll();
+        return repository.findAll();
 
+    }
+    @GetMapping("/{username}")
+    public Usuario getOne(@PathVariable("username") String username){
+        return repository.findByUsername(username);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Integer id){
+        repository.deleteById(id);
+    }
+    @PostMapping()
+    public void postUser(@RequestBody Usuario usuario){
+        repository.save(usuario);
     }
 }
--- src/main/java/dio/web/api/repository/UsuarioRepository.java
+++ src/main/java/dio/web/api/repository/UsuarioRepository.java
@@ -6,33 +6,33 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository // Essa anotação informa ao Spring que essa classe é um Component
+@Repository
 public class UsuarioRepository {
     public void save(Usuario usuario){
         if(usuario.getId()==null)
             System.out.println("SAVE - Recebendo o usuário na camada de repositório");
         else
-            System.out.println("UPDATE - Recebendo o usuário na camada repositório");
+            System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
 
         System.out.println(usuario);
     }
     public void deleteById(Integer id){
-        System.out.println(String.format("DELETE/id - Recebendo o id: %d para"));
+        System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
         System.out.println(id);
     }
-    public List<Usuario> fingAll(){
-        System.out.println("LIST - Listando os usuários do sistema");
+    public List<Usuario> findAll(){
+        System.out.println("LIST - Listando os usários do sistema");
         List<Usuario> usuarios = new ArrayList<>();
         usuarios.add(new Usuario("joabe","password"));
-        usuarios.add(new Usuario("Valverde", "masterpass"));
+        usuarios.add(new Usuario("valverde","masterpass"));
         return usuarios;
     }
     public Usuario findById(Integer id){
-        System.out.println(String.format("FIND/id - Recebendo o id: %d para"));
-        return new Usuario("Joabe","password");
+        System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
+        return new Usuario("gleyson","password");
     }
     public Usuario findByUsername(String username){
-        System.out.println(String.format("FIND/username - recebendo o username"));
-        return new Usuario("Joabe","password");
+        System.out.println(String.format("FIND/username - Recebendo o usernamae: %s para localizar um usuário", username));
+        return new Usuario("gleyson","password");
     }
 }
